200ca86527378d3a7fbe20968e3412ae9c1d859c
mctrl <mctrl@users.noreply.github.com>
2018-12-02 13:36:59 +0000

    Add Day 2

---
+++ day_2.js
@@ -0,0 +1,305 @@
+var listOfBoxes = `revtoubfniyhzsgxdowjwkqglp
+revtcgbfniyhzsvxdomjwkqmlp
+cevtcubfniyhqsgxdoakwkqmlp
+revtcubfniyhzsgxdtavwkqmep
+reutcuboniyhzmgxdoajwkqmlp
+revtcubfniyhzsgxxqajmkqmlp
+rwvtcvbfniyhzsgxdozjwkqmlp
+qevtcbbfniyhzsgxdoljwkqmlp
+rnvtcmbfniyhzsuxdoajwkqmlp
+revtcubfsiyhzsgxdaaewkqmlp
+revtcubfpiyhesgxhoajwkqmlp
+revtcubfnivhzsuxdoljwkqmlp
+retpcubwniyhzsgxdoajwkqmlp
+revrcubfniyhzsgxdyajhkqmlp
+revtcbbfniyhzsixdoajwvqmlp
+revtcubfniyhzsgxdoanmkqmpp
+jevtoubfnuyhzsgxdoajwkqmlp
+rpwtcubfniehzsgxdoajwkqmlp
+revhcubfniyhnsgxdoajwkxmlp
+revtcubfniyhzswxdodjwkqvlp
+reotcubfciyhzsgxdnajwkqmlp
+revtcubfniyhzsgxdbatwsqmlp
+rlvbcubfniyhzssxdoajwkqmlp
+rentcubfnyyhzsgxdozjwkqmlp
+revtcuufniyhasgxdohjwkqmlp
+jevtcubfniyhxsgxdoajwkqwlp
+ravtcubfnryhzfgxdoajwkqmlp
+reltcubfnvyhzsgxdoajwkumlp
+revtrubfnschzsgxdoajwkqmlp
+uevtcubanichzsgxdoajwkqmlp
+revtcubfniyhzdpxdoajwwqmlp
+revtcubfhiyhzsgxdoajgkqplp
+revtcubfniyxzygxdoajwkqmld
+revtcunfniyfzsgxdoajwkqwlp
+reqtcubfniyhzsgxdoajwfqmlj
+revtcubfniyhzagedaajwkqmlp
+revthuefniyhzsgxroajwkqmlp
+revtcrbfodyhzsgxdoajwkqmlp
+revtcubfniyhxsgxdlajwuqmlp
+revtrubfnirhzsgxdokjwkqmlp
+revtiubfniyhzagudoajwkqmlp
+jevtcubfniyhusgxsoajwkqmlp
+reetcubfniyhzsgxdoajvmqmlp
+pestcubfniyhzsgxdoajwkqglp
+revtcubfniyhzsgxdoiowkqalp
+revscubfniyhzsgxdoajwkoplp
+revtcubfnoyhzsgxdlajwkymlp
+rkvtcubfniyhzsgxdoajzkqhlp
+revtuubfniyhzsgxdojjwkqglp
+revtcubmniyhzsgydoajwkzmlp
+revtcybfnijhzsgxvoajwkqmlp
+rxftcubfnkyhzsgxdoajwkqmlp
+gertcubfniyhzsgxjoajwkqmlp
+revtcabfniygzdgxdoajwkqmlp
+levgcubfniyhzsgxdoalwkqmlp
+revtcubfniyhzslxdofjwkqxlp
+revtcybwniyhzsgxdoajwkqmlx
+devtcubpniyhzsgqdoajwkqmlp
+pevtcjbfniyhzsbxdoajwkqmlp
+revtcubfeiehzsgxdoafwkqmlp
+revwcubfniyhzsgxdoawekqmlp
+revtcubfniyussgxdoawwkqmlp
+revtcuafnczhzsgxdoajwkqmlp
+revtaubfniyhusgxdoajwkqilp
+revtcubfnidhzxgxdoajwkqmlt
+revtcubfniyhzsexdmajwnqmlp
+revtcubfnhyhzsgxdwxjwkqmlp
+revtalbfniyhzsgxdoajwbqmlp
+revtcubfniyazsgxdoajwkqcvp
+rcvtcubfniyhzwgxdoajwkqmsp
+revthubfniyhzxgxdoalwkqmlp
+revtcubfniyazsoxgoajwkqmlp
+revtcubkriyhzsgtdoajwkqmlp
+revtcubfniyhzsgxgeajwgqmlp
+heftcubfniypzsgxdoajwkqmlp
+revtclbfniyhzsgxdowjnkqmlp
+revtcubfnifhzsgxdoamwkqmhp
+revncubfniyhzsgxdoxjwiqmlp
+reitcurfniyhzsgxdoajwkrmlp
+revtfmbfniyhzsgxdoajwkqmbp
+revtcubfniahzsgxdoajwkqhtp
+rejtcubfhiyhzsgxdoajwkqmfp
+revtcuxfqiyhzsgxdoajwkqmlh
+revtcuzfniwhzsgxdoajwkqmcp
+revtcubfniyhzsmxdotjwkqmlx
+revtcubfniyhzzgxmoajwkqulp
+revtcuaffiyhzsgxdoajwkqmlj
+revtcxbfniyhzsaxdoajwkqflp
+revtjubfniyhzcrxdoajwkqmlp
+revtcunfniyhzsgxdfajwoqmlp
+revtcubfpiytzswxdoajwkqmlp
+revtcubfniyhzsgxdorjwiqmtp
+oevtcubfniyhzsgidoajwkqmlt
+revccubzniyhztgxdoajwkqmlp
+reircubfniwhzsgxdoajwkqmlp
+revtcubfniyhzsgxhyajwkqvlp
+revtcubfnpyhzsgxdoajwkvblp
+revtduvfniyhzsixdoajwkqmlp
+revtcebfniyhzsgydpajwkqmlp
+revtcubftiyhzsgxwkajwkqmlp
+revtcdbfniyuzsgxdoajlkqmlp
+revtcubfnvydjsgxdoajwkqmlp
+cevtcupfniypzsgxdoajwkqmlp
+revtcubfniyhzsgoeonjwkqmlp
+revtcsbfniyhzsgxdoyjwdqmlp
+revtcubfriyhzugxdoakwkqmlp
+revtcadfniohzsgxdoajwkqmlp
+revrcubfniyhzsguxoajwkqmlp
+ruvtcubfniyhzsxxdoahwkqmlp
+aevtcubfniyhzsgcdoajwkqdlp
+revtcubgniyhzwgxdoajpkqmlp
+revtcubfniyhzegxdoajwkumsp
+rlvtcubzniyhzsgxdoajwkqzlp
+revtfubfniyhzxgbdoajwkqmlp
+revtcubfniyszssxdoajwkymlp
+revtcubfniyhzsgxdoarskzmlp
+rewtcubfniyhzsgxdoajwkpmlh
+revtcubbniyhzsfxdxajwkqmlp
+yeitcubfniyhzsgxdrajwkqmlp
+revtcubfniyhzsrxnoajwkemlp
+revtcuefnqyhzsgxdoajwkqmbp
+revtcubfniyhzsuxdoajwdqnlp
+revtcujfnifhzsgxdoaswkqmlp
+revtcuyfniyhzsgxdoaswklmlp
+reeacubfniyhzsgxdoajwkqmfp
+revtcubvniyhzsgxdoauwkqmls
+revtpubkniyhzsgxdoajvkqmlp
+revtcubfnpyhzsgxdoavnkqmlp
+revtcobfnvyhzsfxdoajwkqmlp
+gevtcubfniyhzsgxcoajwkqmld
+rivtcubfniyhzqgxdpajwkqmlp
+rettgubfngyhzsgxdoajwkqmlp
+revtcuhfccyhzsgxdoajwkqmlp
+rertarbfniyhzsgxdoajwkqmlp
+rhftcybfniyhzsgxdoajwkqmlp
+revtcvjfniyhzsgxboajwkqmlp
+reetcubfnikhzsgxdoajwkqmsp
+revtwubfniyhusgxdoajwkqelp
+revtcdbfniyyzsgxdwajwkqmlp
+revtcurfniyhzsgxduajwkqmtp
+revtcuafneyhzsgxduajwkqmlp
+rpvtcubfziyhzsgxdoajwkqmep
+mevtcubfniyhzssxdoaywkqmlp
+reptcubfniypzsgsdoajwkqmlp
+revtcubfniyhnsgxdoajwcqelp
+revtcutfniyhzsdxdoajwkqmlr
+rpvtcuafniyhzsgxqoajwkqmlp
+revncubfniyhzsgxdoajwkqkpp
+rertcabfniyhzsgxdoejwkqmlp
+revockbfniymzsgxdoajwkqmlp
+revtsubfniyczsgxdoajwkqplp
+revrcubpniyhzbgxdoajwkqmlp
+revrculfniyhzsgxdoajwkrmlp
+revtlubfniyhzsgxdiajhkqmlp
+ravtcubfniyhzsgxdoajwftmlp
+revtcunfxiyhzssxdoajwkqmlp
+revscubfniypzsgxroajwkqmlp
+mevtzubfniyhysgxdoajwkqmlp
+reitcubfniyuzogxdoajwkqmlp
+revycubfniyhwsgxdoajwkqmlg
+revtcubfnyyhzsgxdoajwkomqp
+zevtcutfniyhzsgxcoajwkqmlp
+revtwubfniylzsgxdjajwkqmlp
+oevtcubfniyhzsgxdoaowkzmlp
+revtcubfniyhzsgxdxajwwqclp
+revtcuafniyhzsgxdlacwkqmlp
+revtcubfniyhzsgxdqrjlkqmlp
+revmcubfnvyhzsgxduajwkqmlp
+rgvvcubfniyhzxgxdoajwkqmlp
+revtcubfniyhzsgxdoakwiqmlz
+reztcubfniyhzsgxddajwnqmlp
+revtcrbfnayhzsgxdoajwxqmlp
+revtcuboncyxzsgxdoajwkqmlp
+revtczbfniybxsgxdoajwkqmlp
+yevtcubfniyhcsdxdoajwkqmlp
+reztcmbfniyhzsgxcoajwkqmlp
+restcubfliyhzsbxdoajwkqmlp
+restcubkniyhzsgxdomjwkqmlp
+reokhubfniyhzsgxdoajwkqmlp
+rejtiubfniyhzsnxdoajwkqmlp
+revtcubfuiyjzsgxdoajykqmlp
+revscubfniyhzsixdoajwkqhlp
+revtjuzfniyhzsgxdoajwkqilp
+revtcubfziyhzsgxdoajhgqmlp
+revtcubiniyhzsgldoacwkqmlp
+revtcubfngyhisgxdoajwkqmkp
+ruvtcubfniyhzsgxloajwkqplp
+rtvtcubfniqbzsgxdoajwkqmlp
+revtcubfniyhzegxdffjwkqmlp
+revtcumsniyhzsgxdoajwkqmsp
+rmvtcubfnhyhzsgxsoajwkqmlp
+revtcbbfniyhzsgxdoajwkqzgp
+rebtcjufniyhzsgxdoajwkqmlp
+rephcubfniyhzvgxdoajwkqmlp
+revtcpbfniyxzsgxdoajwkqmls
+revjcubfniyizsgxdoajwkqmcp
+revtcuqfniyhzsgxdoavwkqmdp
+rettcubfniyhzsgxdoojwkqmbp
+rkvtcubfmuyhzsgxdoajwkqmlp
+revtcubcniyhzngxdoajlkqmlp
+revxcubfpiyfzsgxdoajwkqmlp
+revtcubfniyhzsgkkoajwklmlp
+revtcubfniyhzsbxdoajwqqslp
+zecycubfniyhzsgxdoajwkqmlp
+revtcubfniyhzsggaoajwksmlp
+revtcubffiyhzspxdoajwkqmmp
+ruvtcubfniyhzsgxdoajwkamlu
+revtcubfnmyhzsgxjoajwuqmlp
+revtcubfniyhisgxdoajwkqjgp
+revthubfniyhzsgxdoajwkeolp
+ryvtgubfniyhzsgidoajwkqmlp
+reitiubfniyhzsgxdoajwkqmbp
+rektcubfniyhzsfxdoajpkqmlp
+revbcubfniykzsgxdoajwkqwlp
+revzyubfniyhzkgxdoajwkqmlp
+ravtcubfniyhzsgxdoajwkhmap
+revtcubfnfyhzsgxdvpjwkqmlp
+rhvtcnbfnibhzsgxdoajwkqmlp
+revtctbfniywzsgxroajwkqmlp
+revtcubfniyhzsfmdoabwkqmlp
+sevtcubfniynzsgxpoajwkqmlp
+revtcnbfniyhzzgxdoajwzqmlp
+revtcoofniyhzsgxdoajwkqmrp
+revtcubfaiynysgxdoajwkqmlp
+revtlubfniyizsnxdoajwkqmlp
+revtcubfnwyzzsgxdoajwkqmzp
+revtqubfjiyhzsgxdoajwkrmlp
+revtaubfniyhpsgxdoajwkqilp
+revncuufniwhzsgxdoajwkqmlp
+revtcubfngyhisgxdoauwkqmlp
+revtcubynqyhzdgxdoajwkqmlp
+revtcubfniykzsgxdoyjwkqmla
+revttubfniytzsghdoajwkqmlp
+rerzcujfniyhzsgxdoajwkqmlp
+revtcubtniydzsgxdoajwkpmlp
+revecubfniyhzsvxsoajwkqmlp
+revtcuvfniyhzsgsdaajwkqmlp
+revtcubfniyxzsgxdoajtkzmlp
+revtcukfxiyhzsgxdofjwkqmlp
+revtcubfnayhzugxdqajwkqmlp
+revtcbbfniyizsgxdoajwkqmop
+revtcubfnzyhzsgxdoajwoqmpp
+reitcnbfniyqzsgxdoajwkqmlp
+rektcubfniyhzsgxdgijwkqmlp
+revtcubfniyhpsaxdoajdkqmlp
+ckvtcubfniyhzsgxeoajwkqmlp
+revtcubfniyhzsgxdhajzknmlp
+revscubfniyhrsgxdoajwwqmlp
+revtcubfilyhzsgxdpajwkqmlp
+fevtcubyniyhzsgxdoajwkqmpp`.split('\n');
+
+var words = {
+    'two': 0,
+    'three': 0
+};
+listOfBoxes.forEach((value) => {
+    var count = {}
+    var foundTwo = false;
+    var foundThree = false;
+    var letter;
+    for (let index = 0; index < value.length; index++) {
+        letter = value[index]
+        count[letter] = (count[letter] || 0) + 1;
+
+    };
+    for (const key in count) {
+        if (count.hasOwnProperty(key)) {
+            if (count[key] === 2 && !foundTwo) {
+                words.two++;
+                foundTwo = true;
+            } else if (count[key] === 3 && !foundThree) {
+                words.three++;
+                foundThree = true;
+            }
+        }
+    }
+
+
+});
+var result1 = words.two * words.three;
+console.log('Part one - ', result1);
+
+var similarLetters = '';
+listOfBoxes.forEach((currentWord, currentIndex) => {
+    if (similarLetters) return;
+    
+    listOfBoxes.forEach((comparingWord, comparingIndex) => {
+        if (comparingIndex === currentIndex) return;
+        var similarLetterCount = 0;
+        var similarLettersTemp = []
+        for (let index = 0; index < currentWord.length; index++) {
+            if (currentWord[index] === comparingWord[index]) {
+                similarLetterCount++
+                similarLettersTemp.push(currentWord[index])
+            }
+            if (similarLetterCount === currentWord.length - 1) {
+                similarLetters = similarLettersTemp.join('');
+            }
+        }
+    })
+})
+
+console.log('Part two - ', similarLetters)
+
+
